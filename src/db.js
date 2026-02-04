3c6b4ffb0a1c945a55f7ddee358a5d84875ff959
Mustapha tahiru <tahirumustapha13@gmail.com>
2026-02-04 12:29:22 +0000

    update Dockerfile

--- src/db.js
+++ src/db.js
@@ -1,24 +1,3 @@
-// // import pg from "pg";
-// import env from "dotenv";
-// import { Pool } from "pg";
-
-// env.config();
-
-// const db = new Pool({
-//   user: process.env.PG_USER,
-//   host: process.env.PG_HOST,
-//   database: process.env.PG_DATABASE,
-//   password: process.env.PG_PASSWORD,
-//   port: process.env.PG_PORT,
-// });
-// db.connect();
-
-// db.on("error", (err) => {
-//   console.error("Unexpected error on idle client", err);
-//   process.exit(-1);
-// });
-
-// export const query = (text, params) => db.query(text, params);
 
 import { Pool } from "pg";
 import dotenv from "dotenv";
